8efb16a9bc70b893bea1652951c854400e974be4
filippo0000 <filippopolv00@gmail.com>
2025-10-02 16:59:26 +0200

    servlet html logout

---
+++ pure_html/src/main/java/it/polimi/pure_html/filters/TrackChecker.java
@@ -0,0 +1,71 @@
+package it.polimi.pure_html.filters;
+
+import it.polimi.pure_html.DAO.TrackDAO;
+import it.polimi.pure_html.entities.User;
+import it.polimi.pure_html.utils.ConnectionHandler;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.Serial;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Controlla se la traccia appartiene ad un utente loggato.
+ */
+public class TrackChecker extends HttpFilter {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Connection connection = null;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        ServletContext context = filterConfig.getServletContext();
+        connection = ConnectionHandler.openConnection(context);
+    }
+
+    @Override
+    public void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpSession s = req.getSession();
+        User user = (User) s.getAttribute("user");
+        TrackDAO trackDAO = new TrackDAO(connection);
+        int trackId;
+
+        try {
+            trackId = Integer.parseInt(req.getParameter("track_id"));
+            if (trackId < 0)
+                throw new NumberFormatException();
+        } catch (NumberFormatException e) {
+            res.sendError(HttpServletResponse.SC_NOT_FOUND, "TrackId non valido");
+            return;
+        }
+
+        boolean isOwner;
+        try {
+            isOwner = trackDAO.checkTrackOwner(trackId, user);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+
+        if (!isOwner) {
+            res.sendError(HttpServletResponse.SC_NOT_FOUND, "Track non appartiene all'utente");
+            return;
+        }
+
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        ConnectionHandler.closeConnection(connection);
+    }
+}
---
+++ pure_html/src/main/java/it/polimi/pure_html/filters/UserChecker.java
@@ -0,0 +1,40 @@
+package it.polimi.pure_html.filters;
+
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Se Utente è loggato reindirizza a HomePage
+ */
+public class UserChecker implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        String homepage = req.getServletContext().getContextPath() + "/HomePage";
+
+        HttpSession s = req.getSession();
+        if (s.getAttribute("user") != null) {
+            res.sendRedirect(homepage);
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
